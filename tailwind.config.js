26eac289bd261165215c13d8a78209e370cdc634
Saurav Samui <samuisaurav1@gmail.com>
2024-09-08 22:24:33 +0530

    base structure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: "class",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Arial", "sans-serif"],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
